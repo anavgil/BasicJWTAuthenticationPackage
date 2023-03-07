bf40013478c3842fdc0ce57b626cb3353c621331
Alberto Navarro Gil <a.navarro@innovation.es>
2023-03-07 11:10:02 +0100

    Another change

--- JwtAuthenticationPackage/RegisterDependencyInjectionExtension.cs
+++ JwtAuthenticationPackage/RegisterDependencyInjectionExtension.cs
@@ -28,7 +28,6 @@ namespace JwtAuthenticationPackage
                     };
                 });
 
-            serviceContainer.AddAuthorization();
 
             return serviceContainer;
         }
